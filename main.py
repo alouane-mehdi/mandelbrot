7dff55d0285e99422a06a994b453e9e2a650d237
ilyes-chabab <ilyes.chabab@laplateforme.io>
2024-06-13 17:00:21 +0200

    fonction de clicker sur les differentes image pour ouvrir les scripts faites , il manque plus qu'a merge les script

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ class Main:
 
         self.triangle_de_sierpinski = pygame.transform.scale(pygame.image.load('image/triangle_de_sierpinski.png') , (240,200))
         self.mandelbrot = pygame.transform.scale(pygame.image.load('image/mandelbrot.jpeg') , (240,200))
-        self.julia = pygame.transform.scale(pygame.image.load('image/Julia.png') , (240,200))
+        self.julia = pygame.transform.scale(pygame.image.load('image/Julia.png') , (240,240))
         self.flocon_de_koch = pygame.transform.scale(pygame.image.load('image/flocon_de_koch.jpeg') , (240,200)) 
         self.burning_ship = pygame.transform.scale(pygame.image.load('image/burning_ship.png') , (240,200))
+
+        self.rect_triangle= pygame.Rect(0,170,240,200)
+        self.rect_mandelbrot = pygame.Rect(240,170,240,200)
+        self.rect_julia = pygame.Rect(480,170,240,200)
+        self.rect_flocon= pygame.Rect(720,170,240,200)
+        self.rect_burning_ship = pygame.Rect(960,170,240,200)
 
         self.images = [self.triangle_de_sierpinski,self.mandelbrot,self.julia,self.flocon_de_koch,self.burning_ship]
         
@@ -21,7 +27,7 @@ class Main:
         y = 170
         for i in self.images:
             self.screen.blit(i,(x,y))
-            x+=210
+            x+=240
     
 
     def main_pygame(self): 
@@ -30,6 +36,17 @@ class Main:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running =False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.rect_triangle.collidepoint(event.pos):
+                        print("yes")
+                    elif self.rect_triangle.collidepoint(event.pos):
+                        print("yes")
+                    elif self.rect_triangle.collidepoint(event.pos):
+                        print("yes")
+                    elif self.rect_triangle.collidepoint(event.pos):
+                        print("yes")
+                    elif self.rect_triangle.collidepoint(event.pos):
+                        print("yes")
             
             pygame.display.flip()
 
