041387504f4a3802495f9bacd1656b057e6e5e21
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-06-14 17:03:32 +0200

    ajout figure de Mandelbrot et de Sierpinski

---
+++ Sierpinski.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Sierpinski:
+    def __init__(self) -> None:
+        
+        # Dimensions du plot
+        self.width, self.height = 800, 800
+
+        # Nombre d'itérations
+        self.max_iter = 10000
+
+        # Initialisation des points
+        self.points = np.zeros((self.max_iter, 2))
+
+        # Sommets du triangle
+        self.vertices = np.array([[0, 0], [2, 0], [1, np.sqrt(3)]])
+    
+    def compute(self):
+        # Point initial au centre du triangle
+        self.points[0] = [1, np.sqrt(3) / 3]
+        
+        for i in range(1, self.max_iter):
+            vertex = self.vertices[np.random.randint(0, 3)]
+            self.points[i] = (self.points[i - 1] + vertex) / 2
+
+    def display(self):
+        # Affichage avec matplotlib
+        plt.figure(figsize=(10, 10))
+        plt.scatter(self.points[:, 0], self.points[:, 1], s=0.1, color='black')
+        plt.title("Triangle de Sierpinski")
+        plt.axis('equal')
+        plt.axis('off')
+        plt.show()
+    
+    def show(self):
+        self.compute()
+        self.display()
--- main.py
+++ main.py
@@ -1,60 +1,59 @@
 import pygame
-from Julia import *
-
+from Julia import Julia
+from Sierpinski import Sierpinski
+from Mandelbrot import Mandelbrot
 
 class Main:
     def __init__(self) :
-        #init
-        #script des fractales
-        self.julia_script= Julia()
+        # Initialisation des scripts de fractales
+        self.julia_script = Julia()
+        self.sierpinski_script = Sierpinski()
+        self.mandelbrot_script = Mandelbrot()
 
         pygame.init()
 
         self.running = True
-        #screen 
-        self.screen = pygame.display.set_mode((1200,500))
+        # Création de la fenêtre
+        self.screen = pygame.display.set_mode((1200, 500))
 
-        #images
-        self.triangle_de_sierpinski = pygame.transform.scale(pygame.image.load('image/triangle_de_sierpinski.png') , (240,200))
-        self.mandelbrot = pygame.transform.scale(pygame.image.load('image/mandelbrot.jpeg') , (240,200))
-        self.julia = pygame.transform.scale(pygame.image.load('image/Julia.png') , (240,240))
-        self.flocon_de_koch = pygame.transform.scale(pygame.image.load('image/flocon_de_koch.jpeg') , (240,200)) 
-        self.burning_ship = pygame.transform.scale(pygame.image.load('image/burning_ship.png') , (240,200))
+        # Chargement des images
+        self.triangle_de_sierpinski = pygame.transform.scale(pygame.image.load('image/triangle_de_sierpinski.png'), (240, 200))
+        self.mandelbrot = pygame.transform.scale(pygame.image.load('image/mandelbrot.jpeg'), (240, 200))
+        self.julia = pygame.transform.scale(pygame.image.load('image/Julia.png'), (240, 240))
+        self.flocon_de_koch = pygame.transform.scale(pygame.image.load('image/flocon_de_koch.jpeg'), (240, 200)) 
+        self.burning_ship = pygame.transform.scale(pygame.image.load('image/burning_ship.png'), (240, 200))
 
-        self.images = [self.triangle_de_sierpinski,self.mandelbrot,self.julia,self.flocon_de_koch,self.burning_ship]
+        self.images = [self.triangle_de_sierpinski, self.mandelbrot, self.julia, self.flocon_de_koch, self.burning_ship]
 
-        #rectangles
-        self.rect_triangle= pygame.Rect(0,170,240,200)
-        self.rect_mandelbrot = pygame.Rect(240,170,240,200)
-        self.rect_julia = pygame.Rect(480,170,240,200)
-        self.rect_flocon= pygame.Rect(720,170,240,200)
-        self.rect_burning_ship = pygame.Rect(960,170,240,200)
-        
-        
+        # Création des rectangles pour les images
+        self.rect_triangle = pygame.Rect(0, 170, 240, 200)
+        self.rect_mandelbrot = pygame.Rect(240, 170, 240, 200)
+        self.rect_julia = pygame.Rect(480, 170, 240, 200)
+        self.rect_flocon = pygame.Rect(720, 170, 240, 200)
+        self.rect_burning_ship = pygame.Rect(960, 170, 240, 200)
     
     def display_image(self):
-        #fonction pour afficher les images
-        x=0
+        # Fonction pour afficher les images
+        x = 0
         y = 170
         for i in self.images:
-            self.screen.blit(i,(x,y))
-            x+=240
+            self.screen.blit(i, (x, y))
+            x += 240
     
-
     def main_pygame(self): 
-        #fonction pygame
+        # Boucle principale de Pygame
         while self.running: 
             self.display_image()
             for event in pygame.event.get():
-                #fonction pour quitter
+                # Gestion de la fermeture de la fenêtre
                 if event.type == pygame.QUIT:
-                    self.running =False
-                #fonction pour cliquer sur les images et ouvrir les fractales 
+                    self.running = False
+                # Gestion des clics de souris sur les images pour ouvrir les fractales
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if self.rect_triangle.collidepoint(event.pos):
-                        print("yes")
+                        self.sierpinski_script.show()
                     elif self.rect_mandelbrot.collidepoint(event.pos):
-                        print("yes")
+                        self.mandelbrot_script.show()
                     elif self.rect_julia.collidepoint(event.pos):
                         self.julia_script.show()
                     elif self.rect_flocon.collidepoint(event.pos):
@@ -65,9 +64,5 @@ class Main:
             pygame.display.flip()
 
 if __name__ == "__main__":
-    main=Main()
+    main = Main()
     main.main_pygame()
-
-                
-
-
